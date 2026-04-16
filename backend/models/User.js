bf548b2bb9e3fc9f132563d4ab34a804385b3b77
RavaliPotla <238w1a1248@vrsec.ac.in>
2026-04-16 16:22:37 +0530

    day 2 admin,add product

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose= require("mongoose")
+const userSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    mobile:{
+        type:Number,
+        required:true
+    },
+    role:{
+        type:String,
+        requires:true
+    }
+},{timestamps:true})
+
+module.exports=mongoose.model("User",userSchema)
