4363dfdd3e93b266dd11ee377f99fb911f53d024
Ravalipotla <153104901+Ravalipotla@users.noreply.github.com>
2026-04-17 16:43:50 +0530

    Update cartRoute.js

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,44 +1,54 @@
-const express = require("express") 
-const Cart= require("../models/cart")
-const {protect} = require("../middleware/authmiddleware")
-const router=express.Router() 
-router.post("/add",protect,async (req,res)=>{
-    try{
-        const {productId}=req.body
-        let cart=await Cart.findOne({userId:req.user.id})
-        if(!cart){
-            cart=await Cart.create({
-                userId:req.user.id,
-                item:[{productId,quantity:1}]
-            })
-            console.log("if block",cart)
-        }else{
-            console.log("from else",cart)
-            const itemIndex=cart.item.findindex(item=>item.productId.toString()==productId)
-            if(itemIndex>-1){
-                cart.item[itemIndex].quantity+=1
-            }
-            else{
-                cart.item.push({productId,quantity:1})
-            }
-            
-        }
-        await cart.save()
-        return res.status(201).json({message:"Added to cart"}) 
-    }
-    catch(err){
-        return res.status(500).json({message:`error from cart ${err}`})
-    }
-})
+const express = require("express");
+const Cart = require("../models/cart");
+const { protect } = require("../middleware/authmiddleware");
 
-router.get("/",protect,async(req,res)=>{
-    try{
-        const cart=await Cart.findOne({userId:req.user.id}).populate("item.productId")
-        console.log(cart)
-        return res.status(200).json(cart)
+const router = express.Router();
+
+router.post("/add", protect, async (req, res) => {
+    try {
+        const { productId } = req.body;
+
+        let cart = await Cart.findOne({ userId: req.user.id });
+
+        if (!cart) {
+            cart = await Cart.create({
+                userId: req.user.id,
+                item: [{ productId, quantity: 1 }]
+            });
+        } else {
+            if (!cart.item) cart.item = [];
+
+            const itemIndex = cart.item.findIndex(
+                item => item.productId.toString() === productId
+            );
+
+            if (itemIndex > -1) {
+                cart.item[itemIndex].quantity += 1;
+            } else {
+                cart.item.push({ productId, quantity: 1 });
+            }
+        }
+
+        await cart.save();
+
+        return res.status(201).json({ message: "Added to cart" });
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: `error from cart ${err}` });
     }
-    catch(err){
-        return res.status(500).json({message:`error from cart get route ${err}`})
+});
+
+router.get("/", protect, async (req, res) => {
+    try {
+        const cart = await Cart.findOne({ userId: req.user.id })
+            .populate("item.productId");
+
+        return res.status(200).json(cart);
+
+    } catch (err) {
+        return res.status(500).json({ message: `error from cart get route ${err}` });
     }
-})
-module.exports=router
+});
+
+module.exports = router;
